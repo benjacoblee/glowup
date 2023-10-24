6637a575000014d9a42a3b66e97c800cecba1a9e
benjacoblee <benjacoblee@gmail.com>
2023-10-24 16:39:04 +0800

    grouping

--- src/glowup.nim
+++ src/glowup.nim
@@ -2,12 +2,12 @@ import strutils
 import asyncdispatch
 
 type Lowkey = object
-
 type L* = object of CatchableError
 
 const noCap* = true
 const cap* = false 
 const onGod* = true
+const outOfPocket* = Inf
 const lowkey* = Lowkey() 
 
 proc stan*[T](l: Lowkey, args: varargs[T, `$`]) =
@@ -17,37 +17,35 @@ proc stan*[T](l: Lowkey, args: varargs[T, `$`]) =
 proc yeet*(e: ref Exception) =
   raise e
 
+proc letItCook*[T](f: Future[T]): T =
+  while not f.finished:
+    poll()
+  
+  f.read()
+
+proc based*(str: string): string =
+  return str.toLower()
+
+template fr*(statement) =
+  assert statement
+
+template frfr*(statement) =
+  assert statement
+
 template drop*(thing) =
   return thing
 
 template itsGiving*(thing) =
   return thing
 
-template `PERIODT`*() =
+template PERIODT*() =
   break
 
-template `period`*() =
+template period*() =
   break
 
-template `skrt`*() =
+template skrt*() =
   break
 
-proc `letItCook`*[T](f: Future[T]): T =
-  while not f.finished:
-    poll()
-  
-  f.read()
-
-template `fr`*(statement) =
-  assert statement
-
-template `frfr`*(statement) =
-  assert statement
-
-const outOfPocket* = Inf
-
-proc based*(str: string): string =
-  return str.toLower()
-
-template `clapback`*(statement) =
+template clapback*(statement) =
   yield statement
